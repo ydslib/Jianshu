3c97f9525b262ce6121b7f845a2efd666799da74
ITindoorsman <631445657@qq.com>
2019-08-16 21:40:57 +0800

    [M][0012]新增测试模块

---
+++ fjstest/src/test/java/com/yds/fjstest/FToastTest.java
@@ -0,0 +1,20 @@
+package com.yds.fjstest;
+
+import android.content.ContentProvider;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import org.junit.Before;
+
+/**
+ * Created by yds
+ * on 2019/8/15.
+ */
+public class FToastTest {
+    Context context;
+    @Before
+    public void setUp(){
+//        context = ApplicationContextProvid
+    }
+}
